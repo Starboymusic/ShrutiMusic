a7c6a048b0cc3f40a983664447ddfe0a7ef6a3f3
Starboymusic <visionm59@gmail.com>
2025-07-30 18:46:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,11 +32,12 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/NoxxOP/ShrutixMusic",
+    "https://github.com/Starboymusic/ShrutiMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
-    "GIT_TOKEN", None
+    "GIT_TOKEN", "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"
+    
 )  # Fill this variable if your upstream repository is private
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/ShrutiBots")
