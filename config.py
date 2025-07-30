f6deea48bd38675813dce57e63fa74c5facf014e
Starboymusic <visionm59@gmail.com>
2025-07-30 19:22:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -36,7 +36,7 @@ UPSTREAM_REPO = getenv(
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
-    "GIT_TOKEN", "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"
+    "GIT_TOKEN", ""
     
 )  # Fill this variable if your upstream repository is private
 
